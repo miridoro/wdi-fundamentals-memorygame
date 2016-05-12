a486ed9102305b8e3125498a3c72ed8263ca897d
Miriam Wodrich <miriam.wodrich@gmail.com>
2016-05-12 11:14:17 +0100

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
+// setting up variables
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+// setting up conditions
+
+/*
+if(cardOne == cardTwo || cardThree == cardFour){
+	console.log("True");
+}
+else if (cardOne == cardThree || cardOne == cardFour){
+	alert ("false");
+	return false;
+}
+else {
+	alert ("false");
+	return false;
+}
+*/
+
+console.log(cardOne == cardTwo || cardThree == cardFour);
+console.log(cardOne == cardThree || cardOne == cardFour);
+console.log(cardTwo == cardThree || cardTwo == cardFour);
