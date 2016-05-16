df87c66bc8ae3bbe3a801a7940b4cc6a14a9c135
Miriam Wodrich <miriam.wodrich@gmail.com>
2016-05-16 21:50:26 +0100

    create board with javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,19 +4,33 @@ var two = "queen";
 var three = "king";
 var four = "king";
 
-// setting up conditions
+var createBoard = function () {
+	var oldDiv = document.getElementById('gameboard');
+
+for(var i = 0; i < 4; i++) {
+	var newDiv = document.createElement('div');
+	newDiv.className='card';
+	oldDiv.appendChild(newDiv);
+
+	}
+};
+
+createBoard();
 
 
 
 
-if (two !== four || two !== three){
-	alert ("Sorry, try again.");}
-else if (one !== four || one !== three){
-	alert("Sorry, try again.");
-}
-else {
-	alert ("You have a match.");
-}
+
+
+
+// if (two !== four || two !== three){
+// 	alert ("Sorry, try again.");}
+// else if (one !== four || one !== three){
+// 	alert("Sorry, try again.");
+// }
+// else {
+// 	alert ("You have a match.");
+// }
 
 
 // console.log(cardOne == cardTwo || cardThree == cardFour);
