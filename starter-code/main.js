4ce16745203b05c85056aa7d8d30ce1bc009d0af
Miriam Wodrich <miriam.wodrich@gmail.com>
2016-05-19 14:58:08 +0100

    finished memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,15 @@
-// setting up variables
-var one = "queen";
-var two = "queen";
-var three = "king";
-var four = "king";
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+
+
 
 var createBoard = function () {
+
+	
+
 	var oldDiv = document.getElementById('gameboard');
 
 for(var i = 0; i < 4; i++) {
@@ -13,26 +18,83 @@ for(var i = 0; i < 4; i++) {
 	oldDiv.appendChild(newDiv);
 
 	}
+
+
+
+for(var i = 0; i < cards.length; i++){
+	var cardElement = document.getElementsByClassName('card')[i];
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', setImage);
+	cardElement.addEventListener('click', isTwoCards);
+	
+	}
+
+
 };
 
+
+
+var isMatch = function(cardsInPlay) {
+	console.log("isMatch");
+	if(cardsInPlay[0].getAttribute('data-card') === cardsInPlay[1].getAttribute('data-card')){
+		alert("It's a match!");
+
+	}
+	else{
+		alert("Sorry, it's not a match");
+	}
+
+
+};
+
+var isTwoCards = function() {
+	console.log("is2cards");
+	cardsInPlay.push(this);
+
+
+	if(cardsInPlay.length === 2) {
+
+		setTimeout(function() {
+		isMatch(cardsInPlay);
+
+	
+		cardsInPlay[0].innerHTML='';
+		cardsInPlay[1].innerHTML='';
+		cardsInPlay = [];
+	}, 50);
+
+	}
+
+
+};
+
+var setImage = function() {
+
+console.log("setImage:"+this.getAttribute('data-card') );
+
+if(this.getAttribute('data-card') === "queen"){
+	this.innerHTML='<img src="queen.png" alt="Queen of Spades" />';
+}
+else {
+	this.innerHTML='<img src="king.png" alt="King of Spades" />';
+}
+};
+
+
+//Durstenfeld Shuffle
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+shuffleArray(cards);
+console.log(cards);
 createBoard();
 
 
 
-
-
-
-
-// if (two !== four || two !== three){
-// 	alert ("Sorry, try again.");}
-// else if (one !== four || one !== three){
-// 	alert("Sorry, try again.");
-// }
-// else {
-// 	alert ("You have a match.");
-// }
-
-
-// console.log(cardOne == cardTwo || cardThree == cardFour);
-// console.log(cardOne == cardThree || cardOne == cardFour);
-// console.log(cardTwo == cardThree || cardTwo == cardFour);
